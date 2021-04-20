dee380adabaad2ea2cf2fa0a9f088343f689eab2
Elian Becali <elianbecaliaguiar@gmail.com>
2021-04-20 07:39:14 -0400

    :sparkles: feat: add component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,9 @@
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/Logo.svg" alt="Logo spacetraveling" />
+    </header>
+  );
 }
