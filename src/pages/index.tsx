d1dfbd045713d8431e4a48c6596976461e5c20f6
Elian Becali <elianbecaliaguiar@gmail.com>
2021-04-21 22:10:34 -0400

    feat: add page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,13 +26,54 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  console.log({ postsPagination });
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+  return (
+    <main className={styles.homeContainer}>
+      <div className={styles.posts}>
+        <a>
+          <strong>Developing SSG using Node</strong>
+          <p>
+            The complete title should Developing Static Site Generator using
+            NodeJS.
+          </p>
+          <div>
+            <div>
+              <FiCalendar size="0.875rem" />
+              <time>15 Mar 2021</time>
+            </div>
+            <div>
+              <FiUser size="0.875rem" />
+              <span>Elian Becali</span>
+            </div>
+          </div>
+        </a>
+      </div>
+    </main>
+  );
+}
 
-//   // TODO
+// export const getStaticProps: GetStaticProps = async () => {
+//   const prismic = getPrismicClient();
+//   const postsResponse = await prismic.query(
+//     [Prismic.predicates.at('document.type', 'posts')],
+//     {
+//       fetch: ['posts.author', 'posts.title', 'posts.subtitle', 'posts.slug'],
+//       pageSize: 2,
+//     }
+//   );
+
+//   const postsPagination = {
+//     next_page: postsResponse.next_page,
+//     results: postsResponse.results,
+//   };
+
+//   console.log({ postsPagination });
+
+//   return {
+//     props: {
+//       postsPagination,
+//     },
+//   };
 // };
